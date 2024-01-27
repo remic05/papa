90543b6031e63f08e8af7d6a079f6eecfad4591c
Rémi <remi.cyr05@gmail.com>
2024-01-27 18:46:26 -0500

    ici

--- 1.0.py
+++ 1.0.py
@@ -6,4 +6,8 @@
 
 #b) Demander à l'utilisateur un nombre, s'assurer que ce soit un nombre, et crée une liste en ajoutant 1 à la fin.
 # montrer la liste jusqu'a 10 valeurs.
-nb1 = input("Enter first number: ")
+nb1 = input("Entrer un numéro: ")
+
+while nb1.isnumeric():
+    if nb1.isnumeric(): nb1 = int(nb1)
+    else: print("Mettre un nombre")
