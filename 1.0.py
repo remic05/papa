4745803e429d0baa9dafb6845b819e0751c2e64b
Rémi <remi.cyr05@gmail.com>
2024-01-27 16:30:15 -0500

    Regle 1

---
+++ 1.0.py
@@ -0,0 +1,9 @@
+#a)
+# Demander a l'utilisateur de chosiir deux nombres , s'assurer que ce soit un nombre
+# Et calculer la moyenne de ses nombres
+
+
+
+#b) Demander à l'utilisateur un nombre, s'assurer que ce soit un nombre, et crée une liste en ajoutant 1 à la fin.
+# montrer la liste jusqu'a 10 valeurs.
+
