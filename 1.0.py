c43a0e596f02df8488bc3ba2e06d5e860a692d1d
Rémi <remi.cyr05@gmail.com>
2024-01-27 18:35:51 -0500

    input

--- 1.0.py
+++ 1.0.py
@@ -6,4 +6,4 @@
 
 #b) Demander à l'utilisateur un nombre, s'assurer que ce soit un nombre, et crée une liste en ajoutant 1 à la fin.
 # montrer la liste jusqu'a 10 valeurs.
-
+nb1 = input("Enter first number: ")
